7761026d6e97373ba984b98dc4d8be43437ce5dc
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-07-21 18:43:40 +0300

    configureStore added

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-
 const stringMiddleware = () => (next) => (action) => {
-    if (typeof action === 'string'){
+    if (typeof action === 'string') {
         return next({
             type: action
         })
-    } 
-    return next(action);
-} 
+    }
+    return next(action)
+};
 
-
-const store = createStore(
-    combineReducers({heroes, filters}), applyMiddleware(ReduxThunk, stringMiddleware));
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
+})
 
 export default store;
