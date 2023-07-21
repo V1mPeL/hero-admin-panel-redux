5f48290fcb355ef2d5f0e9e8c2bf8923351cd934
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-07-21 18:25:35 +0300

    middleware added

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,20 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
 
-const store = createStore(combineReducers({heroes, filters}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string'){
+        return next({
+            type: action
+        })
+    } 
+    return next(action);
+} 
+
+
+const store = createStore(
+    combineReducers({heroes, filters}), applyMiddleware(ReduxThunk, stringMiddleware));
 
 export default store;
